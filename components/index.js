a9192321c5e2ceebef9daeb8ebf20587007a517d
Jose <jtpgmr@gmail.com>
2022-07-05 14:39:28 -0400

    marketplace UI foundation complete

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard"
+import FocusedStatusBar from "./FocusedStatusBar"
+import { CircleButton, RectButton } from "./Buttons";
+import { NFTTitle, EthPrice, ImageCmp, People, EndDate, SubInfo } from "./NFTInfo";
+import DetailsDescription from "./DetailsDescription";
+import DetailsBid from "./DetailsBid";
+
+export {
+  HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton, NFTTitle, EthPrice, ImageCmp, People, EndDate, SubInfo, DetailsDescription, DetailsBid
+}
